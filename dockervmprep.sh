271fd49316b7646a7ec132a43b53ebd955b14d85
tsmabbas <abbas15_99@yahoo.com>
2022-07-15 08:52:36 +0100

    dockervmprep.sh

---
+++ dockervmprep.sh
@@ -0,0 +1,30 @@
+!# /bin/bash/
+
+#Docker Install
+
+#Update the OS
+
+sudo apt-get update
+
+#Install curl
+sudo apt install -y curl
+
+#Install Docker
+curl https://get.docker.com | sudo bash
+
+#Access Permission
+sudo chmod 666 /var/run/docker.sock
+
+#Add current user to the docker group
+sudo usermod -aG docker $(whoami)
+
+#Docker Compose Install
+# make sure jq & curl is installed
+sudo apt update
+sudo apt install -y curl jq
+# set which version to download (latest)
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# download to /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# make the file executable
+sudo chmod +x /usr/local/bin/docker-compose
