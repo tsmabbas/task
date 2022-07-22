f36819cb5cc0c454f85561680eb64ba898607714
tsmabbas <abbas15_99@yahoo.com>
2022-07-22 18:48:54 +0530

    pytest

--- task/unittest1.py
+++ task/unittest1.py
@@ -6,7 +6,7 @@ try:
 except Exception as e:
     print("Some Modules are Missing {} ".format(e))    
 
-class FlaskTest(unitest.TestCase):
+class FlaskTest(unittest.TestCase):
 
     #Check for response 200
     def test_index(self):
