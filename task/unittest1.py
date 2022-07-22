525a5846e4137219c037e4dc7c18a5d3fc40d766
tsmabbas <abbas15_99@yahoo.com>
2022-07-22 18:04:12 +0530

    pytest

---
+++ task/unittest1.py
@@ -0,0 +1,19 @@
+
+try:
+    From app import app
+    import unittest
+
+except Exception as e:
+    print("Some Modules are Missing {} ".format(e))    
+
+class FlaskTest(unitest.TestCase):
+
+    #Check for response 200
+    def test_index(self)
+        tester = app.test_client(self)
+        response = tester.get("/home")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+if __name__ =="__main__"    
+    unittest.main()
