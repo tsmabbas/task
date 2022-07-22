c4cd9d54cf6f0bb8e95630901bac64e8c9141a7a
tsmabbas <abbas15_99@yahoo.com>
2022-07-22 17:34:46 +0530

    pytest

--- pytest.py
+++ pytest.py
@@ -6,5 +6,10 @@ from task.app import User
  
 @pytest.fixture(scope='module')
 def new_user():
-    user = User('tsmabbas@hotmail.com', 'FlaskIsAwesome')
-    return user
+    # user = User('tsmabbas@hotmail.com', 'FlaskIsAwesome')
+    # return user
+
+    assert new_user.email == 'tsmabbas@hotmail.com'
+    assert new_user.hashed_password != 'FlaskIsAwesome'
+    assert not new_user.authenticated
+    assert new_user.role == 'user'
